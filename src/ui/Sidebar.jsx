7f4fa75e24bd8f964ff45ebf281baa13021c87ca
juananton <juananton.correo@gmail.com>
2023-09-05 12:27:44 +0200

    Build the app layout

---
+++ src/ui/Sidebar.jsx
@@ -0,0 +1,15 @@
+import { styled } from 'styled-components';
+
+const StyledSidebar = styled.aside`
+  background-color: var(--color-grey-0);
+  padding: 1.2rem 4.8rem;
+  border-right: 1px solid var(--color-grey-100);
+
+  grid-row: 1 / -1;
+`;
+
+function Sidebar() {
+  return <StyledSidebar>SIDEBAR</StyledSidebar>;
+}
+
+export default Sidebar;
